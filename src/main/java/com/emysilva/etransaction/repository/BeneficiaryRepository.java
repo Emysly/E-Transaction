9b8efa3568c9a5a6b624c657383f6b5fd327c752
emysilva <emysly12@gmail.com>
2020-11-07 19:38:16 +0100

    created beneficiary repository

---
+++ src/main/java/com/emysilva/etransaction/repository/BeneficiaryRepository.java
@@ -0,0 +1,9 @@
+package com.emysilva.etransaction.repository;
+
+import com.emysilva.etransaction.model.Beneficiary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BeneficiaryRepository extends JpaRepository<Beneficiary, Long> {
+}
