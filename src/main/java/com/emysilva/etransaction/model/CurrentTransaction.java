0c3ad280e751fa8a562cd4b8424dc19f3343ea4b
emysilva <emysly12@gmail.com>
2020-11-07 19:15:00 +0100

    created current transaction model

---
+++ src/main/java/com/emysilva/etransaction/model/CurrentTransaction.java
@@ -0,0 +1,29 @@
+package com.emysilva.etransaction.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrentTransaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date date;
+    private String description;
+    private String type;
+    private String status;
+    private Double amount;
+    private BigDecimal availableBalance;
+
+    @ManyToOne
+    @JoinColumn(name = "current_account_id")
+    private CurrentAccount currentAccount;
+}
