a1a5305f8c34794cd1e6053e556d417aa8a8c67d
emysilva <emysly12@gmail.com>
2020-11-07 18:15:47 +0100

    initial commit

---
+++ src/main/java/com/emysilva/etransaction/ETransactionApplication.java
@@ -0,0 +1,13 @@
+package com.emysilva.etransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emysilva/etransaction/ETransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emysilva.etransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
