ad067c3dcccf2ba5144ba0f27208bcb62642b085
emysilva <emysly12@gmail.com>
2020-11-07 19:14:18 +0100

    created role model

---
+++ src/main/java/com/emysilva/etransaction/model/security/Role.java
@@ -0,0 +1,24 @@
+package com.emysilva.etransaction.model.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Role {
+    @Id
+    private Integer roleId;
+
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<UserRole> userRoles = new HashSet<>();
+
+}
