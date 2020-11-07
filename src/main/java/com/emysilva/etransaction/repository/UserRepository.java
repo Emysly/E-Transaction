f118fd91459019d867da6ef13d0442ab91a42c6e
emysilva <emysly12@gmail.com>
2020-11-07 19:37:56 +0100

    created user repository

---
+++ src/main/java/com/emysilva/etransaction/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.emysilva.etransaction.repository;
+
+import com.emysilva.etransaction.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
