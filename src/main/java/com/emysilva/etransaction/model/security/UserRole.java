1e3268115f2b79c9e6644c452fa6f99149518b63
emysilva <emysly12@gmail.com>
2020-11-07 19:14:12 +0100

    created user role model

---
+++ src/main/java/com/emysilva/etransaction/model/security/UserRole.java
@@ -0,0 +1,27 @@
+package com.emysilva.etransaction.model.security;
+
+import com.emysilva.etransaction.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userRoleId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
