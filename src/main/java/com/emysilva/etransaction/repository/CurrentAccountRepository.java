291a22da405e56319c602f0ee769e8506a414cdd
emysilva <emysly12@gmail.com>
2020-11-07 19:38:27 +0100

    created current account repository

---
+++ src/main/java/com/emysilva/etransaction/repository/CurrentAccountRepository.java
@@ -0,0 +1,9 @@
+package com.emysilva.etransaction.repository;
+
+import com.emysilva.etransaction.model.CurrentAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrentAccountRepository extends JpaRepository<CurrentAccount, Long> {
+}
