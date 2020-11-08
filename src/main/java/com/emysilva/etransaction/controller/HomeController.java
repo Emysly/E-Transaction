4b39c9a86ca5365c6fd641201d23d32aa3fe05a6
emysilva <emysly12@gmail.com>
2020-11-09 00:44:43 +0100

    created home controller

---
+++ src/main/java/com/emysilva/etransaction/controller/HomeController.java
@@ -0,0 +1,83 @@
+package com.emysilva.etransaction.controller;
+
+
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.emysilva.etransaction.model.PrimaryAccount;
+import com.emysilva.etransaction.model.SavingsAccount;
+import com.emysilva.etransaction.model.User;
+import com.emysilva.etransaction.model.security.Role;
+import com.emysilva.etransaction.model.security.UserRole;
+import com.emysilva.etransaction.repository.RoleRepository;
+import com.emysilva.etransaction.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        User user = new User();
+
+        model.addAttribute("user", user);
+
+        return "signup";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signupPost(@ModelAttribute("user") User user,  Model model) {
+
+        if(userService.checkUserExists(user.getUsername(), user.getEmail()))  {
+
+            if (userService.checkEmailExists(user.getEmail())) {
+                model.addAttribute("emailExists", true);
+            }
+
+            if (userService.checkUsernameExists(user.getUsername())) {
+                model.addAttribute("usernameExists", true);
+            }
+
+            return "signup";
+        } else {
+            Set<UserRole> userRoles = new HashSet<>();
+            userRoles.add(new UserRole(user, new Role("ROLE_USER")));
+
+            userService.createUser(user, userRoles);
+//            userService.save(user);
+
+            return "redirect:/";
+        }
+    }
+
+    @RequestMapping("/userFront")
+    public String userFront(Principal principal, Model model) {
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
+        model.addAttribute("savingsAccount", savingsAccount);
+
+        return "userFront";
+    }
+}
