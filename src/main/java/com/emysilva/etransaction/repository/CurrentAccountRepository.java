234ad5841a3cd231a3239a00e713c47ae5a37c06
emysilva <emysly12@gmail.com>
2020-11-08 01:41:41 +0100

    created current account repository

--- src/main/java/com/emysilva/etransaction/repository/CurrentAccountRepository.java
+++ src/main/java/com/emysilva/etransaction/repository/CurrentAccountRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CurrentAccountRepository extends JpaRepository<CurrentAccount, Long> {
+    CurrentAccount findByAccountNumber(Integer accountNumber);
 }
