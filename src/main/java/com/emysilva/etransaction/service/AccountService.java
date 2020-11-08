12ca4fe2890290af19b5739d2bd0e22ab74f7c38
emysilva <emysly12@gmail.com>
2020-11-09 00:43:40 +0100

    modified account service

--- src/main/java/com/emysilva/etransaction/service/AccountService.java
+++ src/main/java/com/emysilva/etransaction/service/AccountService.java
@@ -1,12 +1,12 @@
 package com.emysilva.etransaction.service;
 
-import com.emysilva.etransaction.model.CurrentAccount;
+import com.emysilva.etransaction.model.PrimaryAccount;
 import com.emysilva.etransaction.model.SavingsAccount;
 
 import java.security.Principal;
 
 public interface AccountService {
-    CurrentAccount createCurrentAccount();
+    PrimaryAccount createPrimaryAccount();
     SavingsAccount createSavingsAccount();
     void deposit(String accountType, double amount, Principal principal);
     void withdraw(String accountType, double amount, Principal principal);
